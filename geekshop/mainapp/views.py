362ba26bc045a4a5ef707ad3965c85d9abd437d8
AlexandraKara <awesomekylchik@yandex.ru>
2020-05-15 19:19:03 +0300

    Lesson 1

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here. контроллеры
+
+def main(request):
+    return render(request, 'mainapp/index.html')
+
+def products(request):
+    return render(request, 'mainapp/products.html')
+
+def contact(request):
+    return render(request, 'mainapp/contact.html')
